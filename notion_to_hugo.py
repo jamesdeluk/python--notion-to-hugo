5feb6509cd5820415117a0c2baa10131cbc2d20a
James <jgibbins92@gmail.com>
2020-07-18 17:10:16 +1000

    Images, ToC bugfixes, started subdirectories

--- notion_to_hugo.py
+++ notion_to_hugo.py
@@ -1,29 +1,50 @@
+#!/usr/bin/env python
+
 # for Windows, cwd
+# to do: subdirectories
 
 import glob
 import os
+import shutil
 import re
+from urllib.parse import unquote
+
+# collect files
+files = glob.glob('*.md')
+print(f'{len(files)} files')
+# files_l1 = glob.glob('*', recursive=True)
+# print(files_l1)
+# files_l2 = []
+# for f in files_l1:
+#     print(f)
+#     files_l2.append(glob.glob(f'{f}/*', recursive=True))
+# print(files_l2)
+
+# # using pathlib
+# from pathlib import Path
+# print(Path('.').rglob('*'))
 
 # create file to store processed file in
 try:
-    processed = os.mkdir('processed')
+    os.mkdir('processed')
+except:
+    'file already exists'
+try:
+    os.mkdir('processed/img')
 except:
     'file already exists'
 
-# collect files
-files = glob.glob(f'*.md')
-print(f'{len(files)} files')
 # loop through files
 counter = 1
-for file in files:
+for f in files:
     # extract filename, removing notion note ID
-    title = file.split('\\')[-1][:-3]
+    title = f.split('\\')[-1][:-3]
     title = '-'.join(title.split(' ')[:-1])
     filename = title.lower()
     print(f'({counter}) {filename}.md')
 
     # open file
-    with open(f'{file}', 'r', encoding="utf8") as original: data = original.readlines()
+    with open(f'{f}', 'r', encoding="utf8") as original: data = original.readlines()
     
     # header
     header_line = f'---\n'
@@ -32,17 +53,34 @@ for file in files:
     data.insert(2,header_line)
 
     # internal links
+    print('\n=== Internal Links ===')
     for line in data:
-        regex1 = '\[.*\]\(.*\)' # all links
-        regex2 = '\[.*\]\(.*\.md\)' # markdown files - likely internal
-        regex3 = '\[.*\]\(http.*\)' # external links
+        regex1 = r'\[.*\]\(.*\)' # all links
+        regex2 = r'\[.*\]\(.*\.md\)' # markdown files - likely internal
+        regex3 = r'\[.*\]\(http.*\)' # external links
         if re.match(regex1,line):
             if re.match(regex2,line):
-                print(f'------ {line.rstrip()}')
+                print(f'{line.rstrip()}')
             elif re.match(regex3,line):
                 pass
             else:
-                print(f'------ {line.rstrip()}')
+                print(f'{line.rstrip()}')
+
+    # images
+    print('\n=== Images ===')
+    img_count = 0
+    line_id = 0
+    for line in data:
+        regex_img = r'!\[.*\]\(.*\)'
+        if re.match(regex_img,line):
+            print(line.strip())
+            img_count += 1
+            img_path = unquote(line.replace('[','|').replace(']','|').split('|')[1])
+            filetype = img_path.split('.')[-1]
+            img_new_name = f'img{img_count}'
+            shutil.copy(img_path, f'processed/img/{img_new_name}.{filetype}')
+            data[line_id] = f'![{img_new_name}](img/{img_new_name})\n'
+        line_id += 1
 
     # toc
     toc = []
@@ -50,15 +88,15 @@ for file in files:
         split = line.split(' ')
         if split[0] == '##':
             first = " ".join(split[1:]).strip()
-            second = "-".join(split[1:]).strip().replace('/','').lower()
+            second = "-".join(split[1:]).strip().replace('/','').replace('→','').replace('(','').replace(')','').lower()
             toc.append(f'- [{first}](#{second})\n')
         if split[0] == '###':
             first = " ".join(split[1:]).strip()
-            second = "-".join(split[1:]).strip().replace('/','').lower()
+            second = "-".join(split[1:]).strip().replace('/','').replace('→','').replace('(','').replace(')','').lower()
             toc.append(f'\t- [{first}](#{second})\n')
         if split[0] == '####':
             first = " ".join(split[1:]).strip()
-            second = "\t-".join(split[1:]).strip().replace('/','').lower()
+            second = "\t-".join(split[1:]).strip().replace('/','').replace('→','').replace('(','').replace(')','').lower()
             toc.append(f'- [{first}](#{second})\n')
     for c in reversed(toc):
         data.insert(3,c)
